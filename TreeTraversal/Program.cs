5d3da64d22eb6d42031bc36dbe6fc29cbe13217c
Kerollous fathy <kerollos.fathy.20@gmail.com>
2026-08-13 20:44:33 +0300

    Add Binary Search Tree Class with its Methods

--- TreeTraversal/Program.cs
+++ TreeTraversal/Program.cs
@@ -1,4 +1,5 @@
-﻿using TreeTraversal.Tree;
+﻿using TreeExample.Tree;
+using TreeTraversal.Tree;
 
 namespace TreeTraversal
 {
@@ -6,34 +7,55 @@ namespace TreeTraversal
     {
         static void Main(string[] args)
         {
-            BinaryTree tree = new BinaryTree();
-            tree.Root = new Node(1);
-            tree.Root.Left = new Node(2);
-            tree.Root.Right = new Node(3);
-            tree.Root.Left.Left = new Node(4);
-            tree.Root.Left.Right = new Node(5);
-            tree.Root.Right.Left = new Node(6);
-            tree.Root.Right.Right = new Node(7);
+            //BinaryTree tree = new BinaryTree();
+            //tree.Root = new Node(1);
+            //tree.Root.Left = new Node(2);
+            //tree.Root.Right = new Node(3);
+            //tree.Root.Left.Left = new Node(4);
+            //tree.Root.Left.Right = new Node(5);
+            //tree.Root.Right.Left = new Node(6);
+            //tree.Root.Right.Right = new Node(7);
 
 
-            Console.WriteLine("Pre-Order Traversal");
-            tree.PreOrder(tree.Root);
+            //Console.WriteLine("Pre-Order Traversal");
+            //tree.PreOrder(tree.Root);
 
-            Console.WriteLine();
+            //Console.WriteLine();
 
-            Console.WriteLine("In-Order Traversal");
-            tree.InOrder(tree.Root);
+            //Console.WriteLine("In-Order Traversal");
+            //tree.InOrder(tree.Root);
 
-            Console.WriteLine();
+            //Console.WriteLine();
 
-            Console.WriteLine("Post-Order Traversal");
-            tree.PostOrder(tree.Root);
+            //Console.WriteLine("Post-Order Traversal");
+            //tree.PostOrder(tree.Root);
 
-            Console.WriteLine();
+            //Console.WriteLine();
 
-            Console.WriteLine("Level Order Traversal");
-            tree.LevelOrder(tree.Root);
+            //Console.WriteLine("Level Order Traversal");
+            //tree.LevelOrder(tree.Root);
 
+
+            BinarySearchTree bst = new BinarySearchTree();
+            Node Root = new Node(20);
+            bst.Insert(Root);
+            bst.Insert(new Node(10));
+            bst.Insert(new Node(30));
+            bst.Insert(new Node(25));
+            bst.Insert(new Node(35));
+
+            Console.WriteLine("--- (In-Order) ---");
+            bst.InOrder(bst.Root);
+            Console.WriteLine("\n");
+
+            bst.Delete(20);
+
+            Console.WriteLine("--- after delete node 20 ---");
+            bst.InOrder(bst.Root);
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Is number 20 exist? " + bst.Search(20)); 
+            Console.WriteLine("Is number 25 exist? " + bst.Search(25));
         }
     }
 }
---
+++ TreeTraversal/Tree/BinarySearchTree.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using TreeTraversal.Tree;
+
+namespace TreeExample.Tree
+{
+    public class BinarySearchTree
+    {
+        public Node Root { get; set; }
+        public BinarySearchTree()
+        {
+            Root = null;
+        }
+
+        public void Insert(Node node)
+        {
+            if (Root == null)
+            {
+                Root = node;
+                return;
+            }
+            Node curr = Root;
+            Node parent = null;
+            while (curr != null)
+            {
+                parent = curr;
+                if (node.Item <= curr.Item)
+                {
+                    curr = curr.Left;
+                }
+                else
+                    curr = curr.Right;
+            }
+            if (node.Item <= parent.Item)
+                parent.Left = node;
+            else
+                parent.Right = node;
+        }
+
+
+        public bool Search(int key)
+        {
+            Node curr = Root;
+            while (curr != null)
+            {
+                if (key == curr.Item)
+                    return true;
+                if (key < curr.Item)
+                    curr = curr.Left;
+                else
+                    curr = curr.Right;
+            }
+            return false;
+        }
+
+        public void Delete(int key)
+        {
+            Root = DeleteNode(Root , key);
+        }
+
+        private Node DeleteNode(Node root, int key)
+        {
+            if (root == null)
+                return root;
+            if(key < root.Item)
+            {
+                root.Left = DeleteNode(root.Left, key);
+            }
+            else if(key > root.Item)
+            {
+                root.Right = DeleteNode(root.Right, key);
+            }
+            else
+            {
+                if (root.Left == null && root.Right == null)
+                    return null;
+                else if (root.Left == null)
+                    return root.Right;
+                else if(root.Right == null)
+                    return root.Left;
+                else
+                {
+                    Node successor = GetMinValue(root.Right);
+                    root.Item = successor.Item;
+                    root.Right = DeleteNode(root.Right, successor.Item);
+                }
+            }
+            return root;
+        }
+
+        public Node GetMinValue(Node node)
+        {
+            while(node.Left != null)
+            {
+                node = node.Left;
+            }
+            return node;
+        }
+
+        public void InOrder(Node node)
+        {
+            if (node == null) return;
+            InOrder(node.Left);
+            Console.Write(node.Item + " ");
+            InOrder(node.Right);
+        }
+    }
+}
